2b21de9c9df1db98ae0be95a0f05a7fa2d11b740
SakshamTiwarikk <sakshamtiwari157@gmail.com>
2026-04-02 14:08:21 +0530

    fix cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-    "http://localhost:5173",
-    "https://backend-ledger-ecru.vercel.app/"
+      "http://localhost:5173",
+      "https://backend-ledger-ecru.vercel.app"
     ],
     credentials: true,
   })
