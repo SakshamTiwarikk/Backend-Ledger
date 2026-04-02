f65c94ca92125f98051fc675ba44223b37bb0d1f
SakshamTiwarikk <sakshamtiwari157@gmail.com>
2026-04-02 13:32:13 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 // ✅ ADD THIS BLOCK
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+    "http://localhost:5173",
+    "https://eclectic-medovik-15517b.netlify.app/login"
+    ],
     credentials: true,
   })
 );
